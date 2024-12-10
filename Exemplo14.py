ebd5aca81d4e4285ca3eacff1cb904b7576188d8
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-12-10 17:37:40 -0300

    as

---
+++ Exemplo14.py
@@ -0,0 +1,5 @@
+#A função len() le basicamente o numero de caracteres que tem em uma string
+
+class Main:
+    nome = 'Mario Antonio da Grca'
+    print(len(nome))
--- Projeto 01/Main.py
+++ Projeto 01/Main.py
@@ -10,6 +10,8 @@ from Conta import Conta
 c1 = Cliente('João', '114444-2222') # aqui é registrado a instancia relacionada a classe criada em Cliente
 c2 = Cliente('Maria','115646-1045')
 
-conta = Conta(c1._nome, 6565,0)
+conta = Conta(c1.get_nome(), 6565,0)
 
-print(conta.titular , 'Numero:',conta.numero,'Seu Saldo',conta.saldo)
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
