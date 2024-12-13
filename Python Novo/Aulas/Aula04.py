6c6d6c0a1aecc4959050ace7669002f75dc310bd
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2024-12-13 16:29:48 -0300

    ypu

---
+++ Python Novo/Aulas/Aula04.py
@@ -0,0 +1,31 @@
+#OPERADORES
+# + = SOMA
+5+2 
+
+# - = SUBTRAÇÃO
+5-2
+
+# * = MULTIPLICAÇÃO
+5*2
+
+# / = DIVISÃO
+5/2
+
+# ** = POTENCIA
+5**2
+
+# // = DIVISÃO INTEIRA
+5//2
+
+# % = RESTO DA DIVISÃO
+5%2
+
+#ORDEM DE PRECEDENCIA
+'''
+1 = ()
+2 = **
+3 = * / // %
+4 = + -
+'''
+nome = input('Qual o seu nome?')
+print('Prazer em te conhecer {:^30}!'.format(nome))
