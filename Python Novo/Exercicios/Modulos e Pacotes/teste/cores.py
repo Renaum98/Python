a274c2aed2038ae2fb1b3c613bbf9e8e594e7b38
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-01-30 14:41:07 -0300

    qwe

---
+++ Python Novo/Exercicios/Modulos e Pacotes/teste/cores.py
@@ -0,0 +1,35 @@
+def corVermelha(txt):
+    return f'\033[0;31m{txt}\033[m'
+
+def corVerde(txt):
+    return f'\033[0;32m{txt}\033[m'
+
+def corAmarela(txt):
+    return f'\033[0;33m{txt}\033[m'
+
+def corAzul(txt):
+    return f'\033[0;34m{txt}\033[m'
+
+def corRoxa(txt):
+    return f'\033[0;35m{txt}\033[m'
+
+def corEsmeralda(txt):
+    return f'\033[0;36m{txt}\033[m'
+
+def fundoVermelho(txt):
+    return f'\033[0;30;41m{txt}\033[m'
+
+def fundoVerde(txt):
+    return f'\033[0;30;42m{txt}\033[m'
+
+def fundoAmarelo(txt):
+    return f'\033[0;30;43m{txt}\033[m'
+
+def fundoAzul(txt):
+    return f'\033[0;30;44m{txt}\033[m'
+
+def fundoRoxo(txt):
+    return f'\033[0;30;45m{txt}\033[m'
+
+def fundoEsmeralda(txt):
+    return f'\033[0;30;46m{txt}\033[m'
---
+++ Python Novo/Exercicios/Modulos e Pacotes/teste/teste.py
@@ -0,0 +1,14 @@
+import cores
+p = 'Teste'
+print(cores.corVermelha(p))
+print(cores.corVerde(p))
+print(cores.corAmarela(p))
+print(cores.corAzul(p))
+print(cores.corRoxa(p))
+print(cores.corEsmeralda(p))
+print(cores.fundoVermelho(p))
+print(cores.fundoVerde(p))
+print(cores.fundoAmarelo(p))
+print(cores.fundoAzul(p))
+print(cores.fundoRoxo(p))
+print(cores.fundoEsmeralda(p))
