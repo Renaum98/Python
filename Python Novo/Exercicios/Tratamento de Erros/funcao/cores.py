03e0c41f9aefb86bd35b1208f71a72ac3858edf3
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-02-04 16:03:14 -0300

    upgrade

---
+++ Python Novo/Exercicios/Tratamento de Erros/funcao/cores.py
@@ -0,0 +1,35 @@
+def vermelha(txt):
+    return f'\033[0;31m{txt}\033[m'
+
+def verde(txt):
+    return f'\033[0;32m{txt}\033[m'
+
+def amarela(txt):
+    return f'\033[0;33m{txt}\033[m'
+
+def azul(txt):
+    return f'\033[0;34m{txt}\033[m'
+
+def roxa(txt):
+    return f'\033[0;35m{txt}\033[m'
+
+def esmeralda(txt):
+    return f'\033[0;36m{txt}\033[m'
+
+def fVermelho(txt):
+    return f'\033[0;30;41m{txt}\033[m'
+
+def fVerde(txt):
+    return f'\033[0;30;42m{txt}\033[m'
+
+def fAmarelo(txt):
+    return f'\033[0;30;43m{txt}\033[m'
+
+def fAzul(txt):
+    return f'\033[0;30;44m{txt}\033[m'
+
+def fRoxo(txt):
+    return f'\033[0;30;45m{txt}\033[m'
+
+def fEsmeralda(txt):
+    return f'\033[0;30;46m{txt}\033[m'
