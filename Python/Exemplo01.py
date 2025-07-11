aa7c3651f13fb841ef4992746ddb5989f59ef31d
Renaum98 <167568624+Renaum98@users.noreply.github.com>
2025-07-11 11:24:28 -0300

    Update Exemplo01.py

--- Python/Exemplo01.py
+++ Python/Exemplo01.py
@@ -5,6 +5,6 @@ situação = True
 
 tipo = type (codigo)
 
-print('Codigo:',codigo, "Nome:", nome, 'O salaraioatual é de:', salario)#forma para concatenar utilizando a  virgula ","
+print('Codigo:', codigo, "Nome:", nome, 'O salaraioatual é de:', salario)#forma para concatenar utilizando a  virgula ","
 
 print("Codigo:"+ str (codigo)+ "Nome:"  +nome+"O salaraioatual é de:"+ str (salario)) #esse é um outro metodo de concatenar usando o "+", pra isso precisa ser transformado os valores que não são strings para strings/str
